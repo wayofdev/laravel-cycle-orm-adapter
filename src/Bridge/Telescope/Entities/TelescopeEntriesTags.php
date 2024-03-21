e03e74834ee5a59165f6ccbba1b4fa37d748b378
lotyp <lotyp7@gmail.com>
2024-03-22 00:32:03 +0200

    fix: telescope entities should be same as native migrations

--- src/Bridge/Telescope/Entities/TelescopeEntriesTags.php
+++ src/Bridge/Telescope/Entities/TelescopeEntriesTags.php
@@ -9,13 +9,16 @@ use Cycle\Annotated\Annotation\Entity;
 use Cycle\Annotated\Annotation\ForeignKey;
 use Cycle\Annotated\Annotation\Table\Index;
 
-#[Index(columns: ['entry_uuid', 'tag'])]
+#[Index(columns: ['entry_uuid', 'tag'], unique: true)]
 #[Index(columns: ['tag'])]
 #[ForeignKey(target: TelescopeEntry::class, innerKey: 'entry_uuid', outerKey: 'uuid', action: 'CASCADE')]
-#[Entity(table: 'telescope_entry_tags')]
-class TelescopeEntryTags
+#[Entity(table: 'telescope_entries_tags')]
+class TelescopeEntriesTags
 {
-    #[Column(type: 'uuid', primary: true)]
+    #[Column(type: 'primary')]
+    public int $id;
+
+    #[Column(type: 'uuid')]
     public string $entryUuid;
 
     #[Column(type: 'string')]
--- src/Bridge/Telescope/Entities/TelescopeEntry.php
+++ src/Bridge/Telescope/Entities/TelescopeEntry.php
@@ -6,20 +6,22 @@ namespace WayOfDev\Cycle\Bridge\Telescope\Entities;
 
 use Cycle\Annotated\Annotation\Column;
 use Cycle\Annotated\Annotation\Entity;
+use Cycle\Annotated\Annotation\ForeignKey;
 use Cycle\Annotated\Annotation\Table\Index;
 use DateTimeImmutable;
 
-#[Index(columns: ['batch_id'])]
-#[Index(columns: ['family_hash'])]
-#[Index(columns: ['created_at'])]
-#[Index(columns: ['type', 'should_display_on_index'])]
+#[Index(columns: ['uuid'], unique: true, name: 'telescope_entries_uuid_unique')]
+#[Index(columns: ['batch_id'], name: 'telescope_entries_batch_id_index')]
+#[Index(columns: ['family_hash'], name: 'telescope_entries_family_hash_index')]
+#[Index(columns: ['created_at'], name: 'telescope_entries_created_at_index')]
+#[Index(columns: ['type', 'should_display_on_index'], name: 'telescope_entries_type_should_display_on_index_index')]
 #[Entity(table: 'telescope_entries')]
 class TelescopeEntry
 {
-    #[Column(type: 'bigInteger', primary: true)]
+    #[Column(type: 'primary')]
     public int $sequence;
 
-    #[Column(type: 'uuid', unique: true)]
+    #[Column(type: 'uuid')]
     public string $uuid;
 
     #[Column(type: 'uuid')]
