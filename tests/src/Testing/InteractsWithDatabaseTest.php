5efcc24b14b44ec672581049927fadd8c3610c29
Yauheni Slobadau <yauheni.slobadau@gmail.com>
2026-03-22 22:10:04 +0300

    test(feat): Added new assertions to InteractsWithDatabaseTest.php to ensure it is compatible with iterable tables

--- tests/src/Testing/InteractsWithDatabaseTest.php
+++ tests/src/Testing/InteractsWithDatabaseTest.php
@@ -58,4 +58,65 @@ final class InteractsWithDatabaseTest extends TestCase
     {
         $this->assertDatabaseEmpty('posts');
     }
+
+    #[Test]
+    public function it_asserts_database_has_with_iterable(): void
+    {
+        $em = app(EntityManagerInterface::class);
+        $post = new Post('Title', 'Description');
+        $em->persist($post);
+        $em->run();
+
+        $this->assertDatabaseHas(['posts'], [
+            'id' => $post->id(),
+            'title' => 'Title',
+        ]);
+    }
+
+    #[Test]
+    public function it_asserts_database_count_with_iterable(): void
+    {
+        $em = app(EntityManagerInterface::class);
+        $post1 = new Post('Title 1', 'Description 1');
+        $post2 = new Post('Title 2', 'Description 2');
+        $em->persist($post1);
+        $em->persist($post2);
+        $em->run();
+
+        $this->assertDatabaseCount(['posts'], 2);
+    }
+
+    #[Test]
+    public function it_asserts_database_has_with_entity_class(): void
+    {
+        $em = app(EntityManagerInterface::class);
+        $post = new Post('Title', 'Description');
+        $em->persist($post);
+        $em->run();
+
+        $this->assertDatabaseHas(Post::class, [
+            'id' => $post->id(),
+            'title' => 'Title',
+        ]);
+    }
+
+    #[Test]
+    public function it_asserts_database_has_with_iterable_of_entities(): void
+    {
+        $em = app(EntityManagerInterface::class);
+        $post = new Post('Title', 'Description');
+        $em->persist($post);
+        $em->run();
+
+        $this->assertDatabaseHas([$post], [
+            'id' => $post->id(),
+            'title' => 'Title',
+        ]);
+    }
+
+    #[Test]
+    public function it_asserts_database_empty_with_iterable(): void
+    {
+        $this->assertDatabaseEmpty(['posts']);
+    }
 }
